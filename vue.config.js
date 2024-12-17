e04e9951eb963b8901597b8a8da4f6a3eccc9fd2
MiguelSepulvedaConejeros <msepco@gmail.com>
2024-12-16 21:09:31 -0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,7 @@
-module.exports = {
-  devServer: {
-    proxy: {
-      '/api': { // Todas las llamadas a '/api' pasarán por aquí
-        target: 'https://github.com/MiguelSepulvedaConejeros/ejclase8', // Dirección del servidor
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' } // Opcional: reescribe la ruta
-      }
-    }
-  }
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/https://github.com/MiguelSepulvedaConejeros/ejclase8/'
+    : '/'
+})
