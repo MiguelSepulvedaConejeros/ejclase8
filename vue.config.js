020c42d3e37f7a1ba7be1a51de4f7e97d2943522
MiguelSepulvedaConejeros <msepco@gmail.com>
2024-12-16 21:22:27 -0300

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ejclase8/'
+    ? '/https://github.com/MiguelSepulvedaConejeros/ejclase8/'
     : '/'
 })
